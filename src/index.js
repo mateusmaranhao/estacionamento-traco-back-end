a9cc1901b275a730bbcf56d4afd09ba47925f856
Mateus <mateusmaranhao0210@gmail.com>
2021-10-26 13:29:08 -0300

    feat: integrar com o banco de dados

--- src/index.js
+++ src/index.js
@@ -1,29 +1,16 @@
 import express, { request, response } from "express";
+import { openDatabase } from "./database.js";
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/vehicles', (request, response) =>{
-  const {id} = request.query;
-  const vehicles = [
-    {
-      id: '1',
-      name: '328i e36',
-      owner: 'Mateus César',
-      type: 'car',
-    },
-    {
-      id: '2',
-      name: 'GS-T',
-      owner: 'Rafaella Alvarenga',
-      type: 'car',
-    }
-  ]
-
-  if(id) {
-  response.send(vehicles.filter(vehicle => vehicle.id == id))
-  }
-  response.send(vehicles)
+app.get('/api/vehicles', async (request, response) => {
+  const db = await openDatabase();
+  const vehicles = await db.all(`
+    SELECT * FROM vehicles
+  `);
+  db.close();
+  response.send(vehicles);
 });
 
 app.post('/api/vehicles', (request, response) => {
