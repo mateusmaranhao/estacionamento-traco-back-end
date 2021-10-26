280ef7221d6962beda6e63bb96852dfaaf63d865
Mateus <mateusmaranhao0210@gmail.com>
2021-10-26 08:15:58 -0300

    feat: adicionar query

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/api/ping', (request, response) =>{
-  response.send({
-    message: 'pong'
-  })
-})
+app.get('/api/vehicles', (request, response) =>{
+  const {id} = request.query;
+  const vehicles = [
+    {
+      id: '1',
+      name: '328i e36',
+      owner: 'Mateus',
+      type: 'car',
+    },
+    {
+      id: '2',
+      name: 'GS-T',
+      owner: 'Rafaella Alvarenga',
+      type: 'car',
+    }
+  ]
 
+  if(id) {
+  response.send(vehicles.filter(vehicle => vehicle.id == id))
+  }
+  response.send(vehicles)
+});
 
 app.listen(8000, () => {
   console.log("Olá, mundo. Está é minha primeira API!")
