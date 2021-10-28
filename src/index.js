e00e5789a5b6929642a4a17d1cf465777785ae4a
Mateus <mateusmaranhao0210@gmail.com>
2021-10-28 08:03:41 -0300

    fix: salvar dados na API

--- src/index.js
+++ src/index.js
@@ -12,6 +12,14 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");// dentro do '*' poderia ser qual site poderia fazer a requisiçao.
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+
+  next();
+})
+
 // Endpoints Vehicles
 app.get('/api/vehicles', listVehicles);
 app.post('/api/vehicles', insertVehicles);
